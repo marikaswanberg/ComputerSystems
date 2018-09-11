3807d63d34f9fe70e043f6c29112aeb7c2b72c6e
Marika Swanberg <marlswanb@reed.edu>
2018-09-11 15:31:33 -0700

    All the plots and measurements are done

--- Hw1/plot.py
+++ Hw1/plot.py
@@ -10,10 +10,13 @@ def read_coords(filename):
             y_coords.append(coord)
     return y_coords
 
-python_coords = read_coords("Python_Results.csv")
-c_double_coords = read_coords("C_Double_Results.csv")
-c_float_coords = read_coords("Float_Results.csv")
-c_int8_coords = read_coords("C_int8_t_Results.csv")
+python_coords = read_coords("csv_files/Python_Results.csv")
+c_double_coords = read_coords("csv_files/C_Double_Results.csv")
+c_float_coords = read_coords("csv_files/Float_Results.csv")
+c_int8_coords = read_coords("csv_files/C_int8_t_Results.csv")
+c_int16_coords = read_coords("csv_files/C_int16_t_Results.csv")
+c_int32_coords = read_coords("csv_files/C_int32_t_Results.csv")
+c_int64_coords = read_coords("csv_files/C_int64_Results.csv")
 
 
 import matplotlib.pyplot as plt
@@ -21,5 +24,12 @@ plt.title("Microbenchmark Results")
 plt.plot(list(range(8,25)), c_float_coords, label="C Float")
 plt.plot(list(range(8,25)), c_double_coords, label="C Double")
 plt.plot(list(range(8,25)), c_int8_coords, label="C int8_t")
+plt.plot(list(range(8,25)), c_int16_coords, label="C int16_t")
+plt.plot(list(range(8,25)), c_int32_coords, label="C int32_t")
+plt.plot(list(range(8,25)), c_int64_coords, label="C int64_t")
+#plt.plot(list(range(8,25)), python_coords, label="Python")
+
+plt.xlabel("Object Size Log base 2")
+plt.ylabel("Time in microseconds (apparently off by 10x)")
 plt.legend(loc=4)
 plt.show()
--- Hw1/update_locations.c
+++ Hw1/update_locations.c
@@ -6,25 +6,25 @@
 
 // This function generates a random array of floats of length SIZE,
 // within the real interval [-BOUND, BOUND]
-int8_t* generate_random_list(const long int SIZE, const int BOUND){
+int64_t* generate_random_list(const long int SIZE, const int BOUND){
   assert(SIZE > 0 && "Size must be positive");
   assert(BOUND > 0 && "Bound must be positive");
   // We need to allocate a new array because we need the array past the function call.
-  int8_t* random_array = (int8_t*) malloc(SIZE*sizeof(int8_t));
+  int64_t* random_array = (int64_t*) malloc(SIZE*sizeof(int64_t));
   if (!random_array){
     perror("malloc failed");
     exit(-1);
   }
-  int8_t random_number;
+  int64_t random_number;
   for(int i = 0; i < SIZE; i++){
-    random_number = ((int8_t)rand()/RAND_MAX*(2*BOUND)) - BOUND;
+    random_number = ((int64_t)rand()/RAND_MAX*(2*BOUND)) - BOUND;
     random_array[i] = random_number;
   }
   return random_array;
 }
 
 // Update location by velocity, one time-step
-void update_coords(int8_t* x, int8_t* y, int8_t* z, int8_t* vx, int8_t* vy, int8_t* vz, const long int SIZE){
+void update_coords(int64_t* x, int64_t* y, int64_t* z, int64_t* vx, int64_t* vy, int64_t* vz, const long int SIZE){
   for(int i = 0; i < SIZE; i++){
     x[i] += vx[i];
     y[i] += vy[i];
@@ -34,25 +34,27 @@ void update_coords(int8_t* x, int8_t* y, int8_t* z, int8_t* vx, int8_t* vy, int8
 
 
 // Returns the average runtime for the update_coords function in microseconds
-double timeit(int8_t* x, int8_t* y, int8_t* z, int8_t* vx, int8_t* vy, int8_t* vz, const long int SIZE, const long int ITERS){
+double timeit(int64_t* x, int64_t* y, int64_t* z, int64_t* vx, int64_t* vy, int64_t* vz, const long int SIZE, const long int ITERS){
   struct timespec start, end;
-  double time = 0.0;
+  double  microseconds = 0.0;
+  
+  // time before
   clock_gettime(CLOCK_MONOTONIC, &start);
   
   for(int i = 0; i < ITERS; i++){
     update_coords(x, y, z, vx, vy, vz, SIZE);
-    time += (end.tv_nsec - start.tv_nsec);
-    
   }
+  //time after
   clock_gettime(CLOCK_MONOTONIC, &end);
-  time = (end.tv_sec - start.tv_sec)*1000000.0; // conversion to microseconds
-  time += (end.tv_nsec - start.tv_nsec)*0.001;
-  return time/(ITERS*SIZE);
+  
+  microseconds = (end.tv_sec*1000000 + end.tv_nsec*0.001) - (start.tv_sec*1000000 + start.tv_nsec*0.001); 
+  printf("%f\n", microseconds);
+  return microseconds/(ITERS*SIZE);
 }
 
 // Returns the sum of the elements in a list
-int8_t sum(int8_t* list, const long int size){
-  int8_t sum_total = 0;
+int64_t sum(int64_t* list, const long int size){
+  int64_t sum_total = 0;
   for(int i=0; i<size; i++){
     sum_total += list[i];
   }
@@ -70,18 +72,18 @@ int main(int argc, char* argv[]){
 
   srand(SIZE);
 
-  int8_t* x = generate_random_list(SIZE, 1000);
-  int8_t* y = generate_random_list(SIZE, 1000);
-  int8_t* z = generate_random_list(SIZE, 1000);
-  int8_t* vx = generate_random_list(SIZE, 1);
-  int8_t* vy = generate_random_list(SIZE, 1);
-  int8_t* vz = generate_random_list(SIZE, 1);
+  int64_t* x = generate_random_list(SIZE, 1000);
+  int64_t* y = generate_random_list(SIZE, 1000);
+  int64_t* z = generate_random_list(SIZE, 1000);
+  int64_t* vx = generate_random_list(SIZE, 1);
+  int64_t* vy = generate_random_list(SIZE, 1);
+  int64_t* vz = generate_random_list(SIZE, 1);
 
   double average_time = timeit(x,y,z,vx,vy,vz, SIZE, ITERS);
 
-  int8_t chksum = sum(x,SIZE) + sum(y, SIZE) + sum(z, SIZE);
+  int64_t chksum = sum(x,SIZE) + sum(y, SIZE) + sum(z, SIZE);
   printf("Mean time per coordinate: %fus\n", average_time);
-  printf("Final checksum is: %i\n", chksum);
+  printf("Final checksum is: %li\n", chksum);
   
   // free the memory from the dynamically-allocated arrays
   free(x);
