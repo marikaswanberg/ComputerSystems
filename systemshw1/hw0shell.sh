a89edcb04a75979e62f6d7acabbefa3005866d60
jiljames <jiljames.reed.edu>
2018-09-30 14:53:25 -0700

    Changes to size and iters in shell.

--- systemshw1/hw0shell.sh
+++ systemshw1/hw0shell.sh
@@ -1,17 +1,17 @@
-./buffer 1024 6553600 >> hw0data.csv &&
-./buffer 2048 3276800 >> hw0data.csv &&
-./buffer 4096 1638400 >> hw0data.csv &&
-./buffer 8192 819200 >> hw0data.csv &&
-./buffer 16384 409600 >> hw0data.csv &&
-./buffer 32768 204800 >> hw0data.csv &&
-./buffer 65536 102400 >> hw0data.csv &&
-./buffer 131072 51200 >> hw0data.csv &&
-./buffer 262144 25600 >> hw0data.csv &&
-./buffer 524288 12800 >> hw0data.csv &&
-./buffer 1048576 6400 >> hw0data.csv &&
-./buffer 2097152 3200 >> hw0data.csv &&
-./buffer 4194304 1600 >> hw0data.csv &&
-./buffer 8388608 800 >> hw0data.csv &&
-./buffer 16777216 400 >> hw0data.csv &&
-./buffer 33554432 200 >> hw0data.csv &&
-./buffer 67108864 100 >> hw0data.csv
+./buffer 1024 10000 >> hw0data.csv &&
+./buffer 2048 10000 >> hw0data.csv &&
+./buffer 4096 10000 >> hw0data.csv &&
+./buffer 8192 10000 >> hw0data.csv &&
+./buffer 16384 10000 >> hw0data.csv &&
+./buffer 32768 10000 >> hw0data.csv &&
+./buffer 65536 10000 >> hw0data.csv &&
+./buffer 131072 10000 >> hw0data.csv &&
+./buffer 262144 10000 >> hw0data.csv &&
+./buffer 524288 10000 >> hw0data.csv &&
+./buffer 1048576 10000 >> hw0data.csv &&
+./buffer 2097152 10000 >> hw0data.csv &&
+./buffer 4194304 10000 >> hw0data.csv &&
+./buffer 8388608 10000 >> hw0data.csv &&
+./buffer 16777216 10000 >> hw0data.csv &&
+./buffer 33554432 10000 >> hw0data.csv &&
+./buffer 67108864 10000 >> hw0data.csv
