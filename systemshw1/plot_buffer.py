d8f408c7b5418932954214728ee9acf705dc5f25
Marika Swanberg <marlswanb@reed.edu>
2018-10-01 08:46:38 -0700

    Fixed small error in plots

--- systemshw1/plot_buffer.py
+++ systemshw1/plot_buffer.py
@@ -37,21 +37,28 @@ def take_min(l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11):
         mins.append(min(l1[i],l2[i], l3[i], l4[i], l5[i], l6[i], l7[i], l8[i], l9[i], l10[i], l11[i]))
     return mins
 
+def take_median(l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11):
+    meds = []
+    for i in range(len(l1)):
+        meds.append(statistics.median([l1[i],l2[i], l3[i], l4[i], l5[i], l6[i], l7[i], l8[i], l9[i], l10[i], l11[i]]))
+    return meds
+
 avgs = take_average(coords_1, coords_2, coords_3, coords_4, coords_5, coords_6, coords_7, coords_8, coords_9, coords_10, coords_11)
 mins = take_min(coords_1, coords_2, coords_3, coords_4, coords_5, coords_6, coords_7, coords_8, coords_9, coords_10, coords_11)
+meds = take_median(coords_1, coords_2, coords_3, coords_4, coords_5, coords_6, coords_7, coords_8, coords_9, coords_10, coords_11)
 import matplotlib.pyplot as plt
 
 
-plt.title("Mean Access time in nanoseconds vs. buffer size")
+plt.title("Median Access time in nanoseconds vs. buffer size")
 
 x_axis = []
 for i in range(10,30):
-    x_axis.append(2**(i+1)/1000)
+    x_axis.append(2**(i+1)/1024)
 
-plt.plot(x_axis, avgs)
+plt.plot(x_axis, meds)
 
 #plt.xticks(x_axis)
 
-plt.xlabel("Buffer Size in MB")
+plt.xlabel("Buffer Size in KB")
 plt.ylabel("Time in nanoseconds")
 plt.show()
