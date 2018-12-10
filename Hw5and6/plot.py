f11223d543eaf4fb5215c5c8be7b8860aad221e6
Marika Swanberg <marlswanb@reed.edu>
2018-12-09 21:51:24 -0800

    added README deleted nonsense

--- Hw5and6/plot.py
+++ Hw5and6/plot.py
@@ -22,16 +22,22 @@ def read_x_coords(filename):
             x_coords.append(coord)
     return x_coords
 
-reqs_per_sec = read_x_coords("sustained_throughput2.csv")
-mean_latency = read_y_coords("sustained_throughput2.csv")
+reqs_old = read_x_coords("retime_sustained_throughput.csv")
+mean_latency_old = read_y_coords("retime_sustained_throughput.csv")
+
+reqs_new = read_x_coords("new_sustained_throughput.csv")
+mean_latency_new = read_y_coords("new_sustained_throughput.csv")
 
 
 import matplotlib.pyplot as plt
-plt.title("Mean Response Time vs. Requests Per Seconds")
+plt.title("Mean Response Time vs. Requests Per Second")
 
-plt.plot(reqs_per_sec, mean_latency)
+plt.plot(reqs_old, mean_latency_old, label="old FIFO")
+plt.plot(reqs_new, mean_latency_new, label="new FIFO")
 
-plt.xlabel("Number of Requests Per Second")
+
+plt.xlabel("Requests Per Second")
 plt.ylabel("Mean Response Time (microseconds)")
 plt.xscale('log', basex=2)
+plt.legend(loc=2)
 plt.show()
